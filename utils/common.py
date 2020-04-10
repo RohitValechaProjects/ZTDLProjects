5a417818b04e34d4d7af1635e8237c0307f22b96
RohitValechaProjects <63417037+RohitValechaProjects@users.noreply.github.com>
2020-04-10 11:55:49 -0500

    Initial commit

---
+++ utils/common.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib
+
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+
+pd.set_option("display.max_rows", 13)
+pd.set_option('display.max_columns', 11)
---
+++ utils/matplotlibconf.py
@@ -0,0 +1,8 @@
+from matplotlib.pyplot import rcParams
+from IPython.display import set_matplotlib_formats
+
+rcParams['font.size'] = 14
+rcParams['lines.linewidth'] = 2
+rcParams['figure.figsize'] = (7.5, 5)
+rcParams['axes.titlepad'] = 14
+rcParams['savefig.pad_inches'] = 0.12
